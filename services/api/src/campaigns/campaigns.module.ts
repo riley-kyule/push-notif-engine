8438728e9a9eeb04d8a4c9223f61a287cf950d86
riley-kyule <phillipkyule@gmail.com>
2026-06-18 20:57:29 +0300

    Wire campaign scheduler into module

--- services/api/src/campaigns/campaigns.module.ts
+++ services/api/src/campaigns/campaigns.module.ts
@@ -2,17 +2,20 @@ import { Module } from "@nestjs/common";
 
 import { BrowserPushModule } from "../browser-push/browser-push.module";
 import { DatabaseModule } from "../database/database.module";
+import { SegmentsModule } from "../segments/segments.module";
 import { SitesModule } from "../sites/sites.module";
+import { CampaignsSchedulerService } from "./campaigns-scheduler.service";
 import { CAMPAIGNS_REPOSITORY } from "./campaigns.constants";
 import { CampaignsController } from "./campaigns.controller";
 import { CampaignsService } from "./campaigns.service";
 import { PostgresCampaignsRepository } from "./postgres-campaigns.repository";
 
 @Module({
-  imports: [DatabaseModule, SitesModule, BrowserPushModule],
+  imports: [DatabaseModule, SitesModule, SegmentsModule, BrowserPushModule],
   controllers: [CampaignsController],
   providers: [
     CampaignsService,
+    CampaignsSchedulerService,
     {
       provide: CAMPAIGNS_REPOSITORY,
       useClass: PostgresCampaignsRepository,
